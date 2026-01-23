b80133c0bb4275b99966ce0928dec65cf92d4dce
BStott <Benjamin.Stott@sony.com>
2026-01-23 12:26:06 +0000

    Introduce parameter to opt out of adding FileCheck as in get_default_substitutions, so FileCheck can be added as a daemon tool instead

--- llvm/utils/lit/lit/llvm/config.py
+++ llvm/utils/lit/lit/llvm/config.py
@@ -497,9 +497,8 @@ class LLVMConfig(object):
                 ("%errc_EACCES", "'" + os.strerror(errno.EACCES) + "'")
             )
 
-    def use_default_substitutions(self):
+    def use_default_substitutions(self, add_filecheck=True):
         tool_patterns = [
-            ToolSubst("FileCheck", unresolved="fatal"),
             # Handle these specially as they are strings searched for during
             # testing.
             ToolSubst(
@@ -515,6 +514,8 @@ class LLVMConfig(object):
                 unresolved="fatal",
             ),
         ]
+        if add_filecheck:
+            tool_patterns.append(ToolSubst("FileCheck", unresolved="fatal"))
 
         self.config.substitutions.append(("%python", '"%s"' % (sys.executable)))
 
